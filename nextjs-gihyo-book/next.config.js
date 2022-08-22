9674b44ae58521586b38cfc5b129257feb31d720
Yu <eventbaito@yahoo.co.jp>
2022-08-22 12:17:40 +0900

    コミット

--- nextjs-gihyo-book/next.config.js
+++ nextjs-gihyo-book/next.config.js
@@ -2,9 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents:true,
-  }
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents:true,
+    }
+    if (process.env.NODE_ENV === 'production'){
+      compilerConfig = {
+        ...compilerConfig,
+        //本番環境 react testing libraryを削除
+        reactRemoveProperties: {properties: ['^data-testid$']},
+      }
+    }
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
